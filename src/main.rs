14f247d4186cbcf0a47a55a9f84b6962070db62d
ImVykee <lexy.v.pro@gmail.com>
2026-02-13 22:49:26 +0100

    WIP - does not work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use clap::Parser;
 
 #[derive(Parser)]
@@ -15,9 +16,24 @@ enum Command {
 
 fn main() {
     let statement: Cli = Cli::parse();
+    let content = fs::read_to_string(statement.path).unwrap();
     match statement.command {
-        Command::Search => println!("search command called"),
-        Command::Count => println!("count command called"),
-        _ => println!("invalid command")
+        Command::Search => search(content, statement.arg1),
+        Command::Count => count(content, statement.arg1),
     }
+}
+
+fn count(content: String, pattern: Option<String>) -> Result<i32, String> {
+    let mut counter = 0;
+    let pattern = pattern.ok_or("Pattern is required")?;
+    for line in content.lines() {
+        if line.contains(&pattern){
+            counter += 1;
+        }
+    }
+    Ok(counter)
+}
+
+fn search(content: String, pattern: Option<String>) -> Result<String, String> {
+    match pattern
 }
