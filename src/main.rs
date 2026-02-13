c79edd281d9a3467ac0741035a1578748bf5b7ec
ImVykee <lexy.v.pro@gmail.com>
2026-02-13 22:27:31 +0100

    barebones build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    command: Command,
+    path: std::path::PathBuf,
+    arg1: Option<String>,
+}
+
+#[derive(clap::ValueEnum, Clone)]
+enum Command {
+    Search,
+    Count,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let statement: Cli = Cli::parse();
+    match statement.command {
+        Command::Search => println!("search command called"),
+        Command::Count => println!("count command called"),
+        _ => println!("invalid command")
+    }
 }
