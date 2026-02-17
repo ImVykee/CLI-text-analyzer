1a50af9603afbc867e45343af1d063ddeea25040
Lexy V <lexy.v.pro@gmail.com>
2026-02-17 15:25:45 +0100

    search and count works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use clap::Parser;
+use std::fs;
 
 #[derive(Parser)]
 struct Cli {
@@ -16,24 +16,35 @@ enum Command {
 
 fn main() {
     let statement: Cli = Cli::parse();
-    let content = fs::read_to_string(statement.path).unwrap();
-    match statement.command {
-        Command::Search => search(content, statement.arg1),
-        Command::Count => count(content, statement.arg1),
-    }
+    let content = fs::read_to_string(statement.path).expect("File not found");
+    let result = match statement.command {
+        Command::Search => search(&content, statement.arg1.as_deref()).unwrap(),
+        Command::Count => count(&content, statement.arg1.as_deref())
+            .unwrap()
+            .to_string(),
+    };
+    println!("{}", result);
 }
 
-fn count(content: String, pattern: Option<String>) -> Result<i32, String> {
+fn count(content: &str, pattern: Option<&str>) -> Result<i32, String> {
     let mut counter = 0;
     let pattern = pattern.ok_or("Pattern is required")?;
     for line in content.lines() {
-        if line.contains(&pattern){
+        if line.contains(pattern) {
             counter += 1;
         }
     }
     Ok(counter)
 }
 
-fn search(content: String, pattern: Option<String>) -> Result<String, String> {
-    match pattern
+fn search(content: &str, pattern: Option<&str>) -> Result<String, String> {
+    let mut line_count = 0;
+    let pattern = pattern.ok_or("Pattern is required")?;
+    for line in content.lines() {
+        line_count += 1;
+        if line.contains(pattern) {
+            break;
+        }
+    }
+    Ok(format!("{} found at line {}", pattern, line_count))
 }
